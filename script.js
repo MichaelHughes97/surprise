ae2329d36279f13d454a8a58cf32d724473d0941
MichaelHughes97 <162193170+MichaelHughes97@users.noreply.github.com>
2026-06-30 23:20:08 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+// MUDA A SENHA AQUI
+const correctPassword = "maracuja";
+
+
+// PASSWORD
+const passwordScreen = document.getElementById("passwordScreen");
+const mainContent = document.getElementById("mainContent");
+
+const passwordInput = document.getElementById("passwordInput");
+const passwordBtn = document.getElementById("passwordBtn");
+
+const wrongPassword = document.getElementById("wrongPassword");
+
+passwordBtn.addEventListener("click", () => {
+
+    if (passwordInput.value === correctPassword) {
+
+        passwordScreen.style.display = "none";
+
+        mainContent.classList.remove("hidden");
+
+    } else {
+
+        wrongPassword.classList.remove("hidden");
+
+    }
+
+});
+
 const button = document.getElementById("surpriseBtn");
 const surprise = document.getElementById("surprise");
 const music = document.getElementById("music");
